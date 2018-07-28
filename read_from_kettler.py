90523895b5ef49752d3de0d2e7b73d2e139cde6c
Joe Kearney <jjk@soundcloud.com>
2018-07-28 12:54:05 +0200

    Improve serial port detection behaviour

--- read_from_kettler.py
+++ read_from_kettler.py
@@ -4,41 +4,48 @@ import os
 
 import serial
 import re
+import socket
+from ant_model import PowerModel
 
-def findKettlerPath():
-    "returns serial.Serial instance for the first Kettler serial port found"
-    fail=serial.serialutil.SerialException
+
+def find_kettler(debug):
+    "returns a Kettler instance for the first Kettler serial port found that replies to ID and ST"
 
     print "Looking for serial ports for a Kettler device..."
 
-    candidates = [f for f in os.listdir("/dev/") if re.match(r'cu\.KETTLER[0-9A-Z]+-SerialPort', f)]
+    candidates = [f for f in os.listdir("/dev/") if
+                  re.match(r'cu\.KETTLER[0-9A-Z]+-SerialPort', f)]
 
-    print "Found candidates: " + str(candidates)
+    print "Found %s candidates" % len(candidates)
 
     for c in candidates:
+        print "Trying: [%s]..." % c
         try:
             serial_name = "/dev/" + c
-            serialPort = serial.Serial(serial_name, timeout=1)
-            return serialPort
-        except fail,e:
+            serial_port = serial.Serial(serial_name, timeout=1)
+            kettler = Kettler(serial_port, debug)
+            kettler_id = kettler.getId()
+            if len(kettler_id) > 0:
+                print "Connected to Kettler [%s] at [%s]" % (kettler_id, serial_name)
+                return kettler
+        except Exception as e:
             print e
             pass
 
-    raise Exception, "No serial port found"
+    raise Exception("No serial port found")
 
-from ant_model import PowerModel
 
 class Kettler():
-    def __init__(self, serialPort, debug = False):
-        self.serialPort = serialPort
+    def __init__(self, serial_port, debug=False):
+        self.serial_port = serial_port
         self.debug = debug
         self.GET_ID = "ID\r\n"
         self.GET_STATUS = "ST\r\n"
 
     def rpc(self, message):
-        self.serialPort.write(message)
-        self.serialPort.flush()
-        response = self.serialPort.readline().rstrip() # rstrip trims trailing whitespace
+        self.serial_port.write(message)
+        self.serial_port.flush()
+        response = self.serial_port.readline().rstrip()  # rstrip trims trailing whitespace
         return response
 
     def getId(self):
@@ -62,19 +69,18 @@ class Kettler():
             return None
 
     def close(self):
-        closeSafely(self.serialPort)
+        close_safely(self.serial_port)
 
 
-def closeSafely(thing):
+def close_safely(thing):
     try:
         thing.close()
     except Exception as e:
         print "Failed to close [%s]: %s" % (str(thing), str(e))
 
-import socket
 
 class TcpWriter():
-    def __init__(self, host, port, debug, retries = 5):
+    def __init__(self, host, port, debug, retries=5):
         self.host = host
         self.port = port
         self.debug = debug
@@ -91,16 +97,16 @@ class TcpWriter():
     def __doWrite(self, message):
         self.tcpSocket.send("%s\n" % message)
 
-    def __resend(self, message, retryCount):
-        if (retryCount <= self.totalRetries):
+    def __resend(self, message, retry_count):
+        if retry_count <= self.totalRetries:
             try:
-                self.debugPrint("Retries remaining: [%s]..." % retryCount)
+                self.debugPrint("Retries remaining: [%s]..." % retry_count)
                 self.__connect()
                 self.__doWrite(message)
                 return True
             except Exception as e:
                 self.debugPrint("Failed to send with exception %s" % str(e))
-                self.__resend(message, retryCount + 1)
+                self.__resend(message, retry_count + 1)
         else:
             return False
 
@@ -115,24 +121,27 @@ class TcpWriter():
                 print message
                 raise Exception(message)
 
+
 TCP_TARGET_HOST = "192.168.1.195"
 TCP_TARGET_PORT = 1234
 DEBUG = False
 
-if __name__=="__main__":
-    serialPort = findKettlerPath()
-    print 'Kettler found at [%s @ %s]' % (serialPort.name, serialPort.port)
-
-    kettler = Kettler(serialPort, DEBUG)
+if __name__ == "__main__":
+    kettler = find_kettler(DEBUG)
+    print 'Kettler found at [%s]' % kettler.serial_port
 
     kettlerId = kettler.getId()
-    print "Connected to Kettler with ID: [%s]" % kettlerId
+    if len(kettlerId) > 0:
+        print "Connected to Kettler with ID: [%s]" % kettlerId
+    else:
+        raise Exception("Kettler didn't reply with an ID")
 
     try:
         writer = TcpWriter(TCP_TARGET_HOST, TCP_TARGET_PORT, DEBUG)
         print "Connected to Ant+ adapter at [%s:%s]" % (TCP_TARGET_HOST, TCP_TARGET_PORT)
 
-        print "Streaming data from Kettler at [%s] to Ant+ adapter at [%s:%s]..." % (kettlerId, TCP_TARGET_HOST, TCP_TARGET_PORT)
+        print "Streaming data from Kettler at [%s] to Ant+ adapter at [%s:%s]..." \
+              % (kettlerId, TCP_TARGET_HOST, TCP_TARGET_PORT)
 
         while True:
             model = kettler.readModel()
@@ -143,5 +152,5 @@ if __name__=="__main__":
         closeSafely(kettler)
     except KeyboardInterrupt:
         print "Closing connection to Kettler [%s]" % kettlerId
-        closeSafely(writer.tcpSocket)
-        closeSafely(kettler)
+        close_safely(writer.tcpSocket)
+        close_safely(kettler)
